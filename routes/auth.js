20bd951afb20738983ad5690ae396d3853a672ca
kaushal3001 <kaushal.kirpekar@gmail.com>
2023-04-08 19:59:32 +0530

    verify Token

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
-
+const jwt = require('jsonwebtoken');
 
 //Register
 router.post('/register', async(req, res) => {
@@ -31,7 +31,13 @@ router.post('/login', async(req, res) => {
         const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
         originalPassword!==req.body.password && res.status(401).json('Wrong username or password!');
 
-        res.status(200).json(user);
+        const accessToken = jwt.sign({
+            id: user._id,
+            isAdmin: user.isAdmin
+        }, process.env.JWT_SEC, {expiresIn: "3d"});
+
+        const{password, ...others} = user._doc;
+        res.status(200).json({...others, accessToken});
 
 
     } catch (err) {
